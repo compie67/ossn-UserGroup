ae1b9ce35746f565f2f0e78c0569b6739b89edb0
compie67 <compie67@gmail.com>
2025-04-05 10:15:34 +0200

    Update ossn.nl.php

--- locale/ossn.nl.php
+++ locale/ossn.nl.php
@@ -1,1 +1,12 @@
-
+<?php
+$nl = array(
+    'usergroups:title' => 'Groepen van %s',
+    'usergroups:members' => 'Leden: %s',
+    'usergroups:privacy:unknown' => 'Onbekend',
+    'groups' => 'Groepen',
+    'Sorteren op:' => 'Sorteren op:',
+    'Nieuwste eerst' => 'Nieuwste eerst',
+    'Oudste eerst' => 'Oudste eerst',
+    'Meeste leden' => 'Meeste leden',
+);
+ossn_register_languages('nl', $nl);
