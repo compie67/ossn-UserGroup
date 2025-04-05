154bb239b64fcc511125fd362524724af2c8d068
compie67 <compie67@gmail.com>
2025-04-05 10:27:47 +0200

    Create ossn.en.php

---
+++ locale/ossn.en.php
@@ -0,0 +1,12 @@
+<?php
+$en = array(
+    'usergroups:title' => 'Groups of %s',
+    'usergroups:members' => 'Members: %s',
+    'usergroups:privacy:unknown' => 'Unknown',
+    'groups' => 'Groups',
+    'Sorteren op:' => 'Sort by:',
+    'Nieuwste eerst' => 'Newest first',
+    'Oudste eerst' => 'Oldest first',
+    'Meeste leden' => 'Most members',
+);
+ossn_register_languages('en', $en);
