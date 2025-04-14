5350f01be55d96dc4e1f96fc052c73b1bb0360e6
compie67 <compie67@gmail.com>
2025-04-14 18:51:27 +0200

    Update ossn.nl.php
    optie voor geen cover

--- locale/ossn.nl.php
+++ locale/ossn.nl.php
@@ -1,12 +1,17 @@
 <?php
 $nl = array(
-    'usergroups:title' => 'Groepen van %s',
-    'usergroups:members' => 'Leden: %s',
-    'usergroups:privacy:unknown' => 'Onbekend',
-    'groups' => 'Groepen',
     'ossn:user:groups:sort' => 'Sorteren op:',
     'ossn:user:groups:newest' => 'Nieuwste eerst',
     'ossn:user:groups:oldest' => 'Oudste eerst',
-    'ossn:user:groups:members' => 'Meeste leden',	
+    'ossn:user:groups:members' => 'Meeste leden',
+    'ossn:user:groups:az' => 'Groepsnaam A-Z',
+    'ossn:user:groups:za' => 'Groepsnaam Z-A',
+    'usergroups:no_groups' => 'Deze gebruiker beheert nog geen groepen.',
+    'usergroups:privacy:unknown' => 'Onbekende privacy-instelling',
+    'usergroups:showcovers' => 'Toon covers:',
+    'usergroups:apply' => 'Toepassen',
+    'usergroups:members' => '%s leden',
+    'yes' => 'Ja',
+    'no' => 'Nee',
 );
 ossn_register_languages('nl', $nl);
