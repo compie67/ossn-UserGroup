8d79ef03bb7e1a021657152b32eba05f722fbbc1
compie67 <compie67@gmail.com>
2025-04-14 18:50:50 +0200

    Update ossn.en.php
    added option to display no-cover

--- locale/ossn.en.php
+++ locale/ossn.en.php
@@ -1,12 +1,17 @@
 <?php
 $en = array(
-    'usergroups:title' => 'Groups of %s',
-    'usergroups:members' => 'Members: %s',
-    'usergroups:privacy:unknown' => 'Unknown',
-    'groups' => 'Groups',
-    'ossn:user:groups:sort'  => 'Sort by:',
+    'ossn:user:groups:sort' => 'Sort by:',
     'ossn:user:groups:newest' => 'Newest first',
     'ossn:user:groups:oldest' => 'Oldest first',
     'ossn:user:groups:members' => 'Most members',
+    'ossn:user:groups:az' => 'Group name A-Z',
+    'ossn:user:groups:za' => 'Group name Z-A',
+    'usergroups:no_groups' => 'This user has not created any groups.',
+    'usergroups:privacy:unknown' => 'Unknown privacy setting',
+    'usergroups:showcovers' => 'Show covers:',
+    'usergroups:apply' => 'Apply',
+    'usergroups:members' => '%s members',
+    'yes' => 'Yes',
+    'no' => 'No',
 );
 ossn_register_languages('en', $en);
